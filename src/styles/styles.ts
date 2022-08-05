473d14c4093cc39893d91b5b7f51dca1b4ea5767
Thiago Cabral Carboneri <thiago.carboneri@ringa.com.br>
2022-08-04 22:05:38 -0300

    Creates styles.ts and implements loginInputStyles logic

---
+++ src/styles/styles.ts
@@ -0,0 +1,13 @@
+import { makeStyles } from '@material-ui/core';
+
+export const useStyles = makeStyles({
+  loginInputStyles: {
+    '& .MuiInputLabel-root.Mui-focused': {
+      color: '#ABA8A8'
+    },
+    '& .MuiInput-root:after': {
+      borderBottom: '2px solid #ABA8A8'
+    }
+  },
+});
+
