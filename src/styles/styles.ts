4c0fd851f103b0aaf3da8c9882958a39e0722831
Thiago Cabral Carboneri <thiago.carboneri@ringa.com.br>
2022-08-05 12:20:57 -0300

    Implements btn styles

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -9,5 +9,16 @@ export const useStyles = makeStyles({
       borderBottom: '2px solid #ABA8A8'
     },
   },
+  btn: {
+    padding: '15px',
+    fontSize: '3vmin',
+    fontWeight: 700,
+    borderRadius: '10px',
+    border: 'none',
+    textTransform: 'uppercase',
+    '&:hover': {
+      cursor: 'pointer',
+    }
+  }
 });
 
