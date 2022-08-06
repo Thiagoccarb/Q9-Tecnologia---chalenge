32151dd9cdd6dabc9ccc4ea5b1009d2e569d5845
Thiago Cabral Carboneri <thiago.carboneri@ringa.com.br>
2022-08-06 11:36:43 -0300

    Implements div cards-container logic

--- src/pages/BreedsMenu/Menu.tsx
+++ src/pages/BreedsMenu/Menu.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react';
+import Grid from '@mui/material/Grid';
 
 import { fetchBreedList } from '../../API/list';
 
 export default function BreedsMenu() {
   const [showBreedsMenu, setShowBreedsMenu] = React.useState<boolean>(false);
-  const [breedPictures, setPictures] = React.useState<string[]>([]);
+  const [breedPictures, setPictures] = React.useState<
+    { breed: string, list: string[] }
+  >({ breed: '', list: [] });
+
+  const { list: picturesArr } = breedPictures;
 
   const handleShowBreedsMenu = () => setShowBreedsMenu(!showBreedsMenu);
 
@@ -18,25 +23,50 @@ export default function BreedsMenu() {
   }, []);
 
   return (
-    <section
-      className='menu'
-    >
-      <ul
-        className={showBreedsMenu ? 'on' : ''}
+    <>
+      <section
+        className='menu'
       >
-        <li>chihuahua</li>
-        <li>husky</li>
-        <li>labrador</li>
-        <li>pug</li>
-      </ul>
-      <button
-        className='hamburger-menu'
-        onClick={handleShowBreedsMenu}
+        <ul
+          className={showBreedsMenu ? 'on' : ''}
+        >
+          <li>chihuahua</li>
+          <li>husky</li>
+          <li>labrador</li>
+          <li>pug</li>
+        </ul>
+        <button
+          className='hamburger-menu'
+          onClick={handleShowBreedsMenu}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+      </section>
+      <Grid
+        container
+        id='cards-container'
       >
-        <span></span>
-        <span></span>
-        <span></span>
-      </button>
-    </section>
+        {
+          picturesArr.map((picture, i) => (
+            <Grid
+              item
+              key={i}
+              sm={2}
+              md={2}
+            >
+              <div
+                className='thumb'
+                style={{
+                  backgroundImage: 'url(' + picture + ')',
+                }}
+              >
+              </div>
+            </Grid>
+          ))
+        }
+      </Grid>
+    </>
   );
 }
