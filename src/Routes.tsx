dbd67fc7b6282d9b23e48270a0d14f74823485e6
Thiago Cabral Carboneri <thiago.carboneri@ringa.com.br>
2022-08-06 00:07:44 -0300

    Implements InitialPage route logic in Routes.tsx

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { LoginPage } from './pages/login'
+import { LoginPage, InitialPage } from './pages/login'
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<InitialPage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
