c78fcc7a87cb539f924ff52c688e261fd01a9622
Thiago Cabral Carboneri <thiago.carboneri@ringa.com.br>
2022-08-06 14:51:50 -0300

    Refactors BreedList and BreedsMenu to improve logic

--- src/pages/BreedsMenu/BreedList.tsx
+++ src/pages/BreedsMenu/BreedList.tsx
@@ -1,14 +1,62 @@
 import * as React from 'react';
+import Grid from '@mui/material/Grid';
 import { useOutletContext } from 'react-router-dom';
 
+import { fetchBreedList } from '../../API/list';
 
 export default function BreedList() {
   const breed = useOutletContext<string>();
+  const [breedPictures, setPictures] = React.useState<
+    { breed: string, list: string[] }
+  >({ breed: '', list: [] });
+
+  const { list: picturesArr } = breedPictures;
+
+  const getBreedList = React.useCallback(async () => {
+    const data = await fetchBreedList(breed);
+    setPictures(data);
+  }, [breed]);
+
+  React.useEffect(() => {
+    getBreedList();
+  }, [getBreedList]);
+
   return (
     <>
-      <h1
-        className='title'
-      >{breed}</h1>
+      <Grid
+        container
+      >
+        <Grid
+          container
+          id='cards-container'
+          columnGap={0}
+          rowGap={2}
+        >
+          <h1
+            className='title'
+          >{breed}</h1>
+          {
+            picturesArr.map((picture, i) => (
+              <Grid
+                item
+                key={i}
+                xs={12}
+                sm={4}
+                md={3}
+                lg={3}
+              >
+                <div
+                  className='thumb'
+                  style={{
+                    backgroundImage: 'url(' + picture + ')',
+                  }}
+                >
+                </div>
+              </Grid>
+            ))
+          }
+        </Grid>
+      </Grid>
     </>
   );
 }
--- src/pages/BreedsMenu/BreedsMenu.tsx
+++ src/pages/BreedsMenu/BreedsMenu.tsx
@@ -1,37 +1,20 @@
 import * as React from 'react';
-import Grid from '@mui/material/Grid';
 import { useNavigate, Outlet, useLocation } from 'react-router-dom';
-
-import { fetchBreedList } from '../../API/list';
 
 export default function BreedsMenu() {
   const navigate = useNavigate();
   const { pathname } = useLocation();
   const [showBreedsMenu, setShowBreedsMenu] = React.useState<boolean>(false);
-  const [breedPictures, setPictures] = React.useState<
-    { breed: string, list: string[] }
-  >({ breed: '', list: [] });
+
 
   const [, breed] = pathname.split('/');
 
-  const { list: picturesArr } = breedPictures;
-
   const handleShowBreedsMenu = () => setShowBreedsMenu(!showBreedsMenu);
-
-  const getBreedList = React.useCallback(async () => {
-    const data = await fetchBreedList(breed);
-    setPictures(data);
-  }, [breed]);
-
 
   const redirect = async ({ target }: any) => {
     const { innerText: breedName } = target as HTMLElement;
     navigate(`/${breedName.toLowerCase()}`)
   }
-
-  React.useEffect(() => {
-    getBreedList();
-  }, [getBreedList]);
 
   return (
     <>
@@ -63,38 +46,7 @@ export default function BreedsMenu() {
           <span></span>
         </button>
       </section>
-      <Grid
-        container
-      >
-        <Grid
-          container
-          id='cards-container'
-          columnGap={0}
-          rowGap={2}
-        >
-          <Outlet context={breed} />
-          {
-            picturesArr.map((picture, i) => (
-              <Grid
-                item
-                key={i}
-                xs={12}
-                sm={4}
-                md={3}
-                lg={3}
-              >
-                <div
-                  className='thumb'
-                  style={{
-                    backgroundImage: 'url(' + picture + ')',
-                  }}
-                >
-                </div>
-              </Grid>
-            ))
-          }
-        </Grid>
-      </Grid>
+      <Outlet context={breed} />
     </>
   );
 }
