d28eba6aae41d753ff8041242fa4d9f729ae8767
Thiago Cabral Carboneri <thiago.carboneri@ringa.com.br>
2022-08-06 00:18:30 -0300

    Implements Menu route logic in Routes.tsx

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { LoginPage, InitialPage } from './pages/login'
+import { Menu } from './pages/BreedsMenu';
 
 const AppRoutes = () => {
   return (
@@ -9,6 +10,7 @@ const AppRoutes = () => {
       <Routes>
         <Route path="/" element={<InitialPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/breeds" element={<Menu />} />
       </Routes>
     </BrowserRouter>
   );
