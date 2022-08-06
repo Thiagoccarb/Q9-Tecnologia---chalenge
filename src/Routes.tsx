6dd0b183aa5c17bcc7ab3c8a1c13b90d0a07dbe0
Thiago Cabral Carboneri <thiago.carboneri@ringa.com.br>
2022-08-06 13:19:56 -0300

    Implements BreedList route logic

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { LoginPage, InitialPage } from './pages/login'
-import { Menu } from './pages/BreedsMenu';
+import { Menu, BreedList } from './pages/BreedsMenu';
+
 
 const AppRoutes = () => {
   return (
@@ -10,7 +11,9 @@ const AppRoutes = () => {
       <Routes>
         <Route path="/" element={<InitialPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/breeds" element={<Menu />} />
+        <Route path="/breeds" element={<Menu />} >
+          <Route path=":id" element={<BreedList />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
