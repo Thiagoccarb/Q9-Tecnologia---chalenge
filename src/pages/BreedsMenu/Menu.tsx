367d464e58ec9284bd584535f2cbccf136f3e0c8
Thiago Cabral Carboneri <thiago.carboneri@ringa.com.br>
2022-08-06 12:04:19 -0300

    Implements div cards-container css and div thumb css

--- src/pages/BreedsMenu/Menu.tsx
+++ src/pages/BreedsMenu/Menu.tsx
@@ -47,14 +47,18 @@ export default function BreedsMenu() {
       <Grid
         container
         id='cards-container'
+        columnGap={0}
+        rowGap={2}
       >
         {
           picturesArr.map((picture, i) => (
             <Grid
               item
               key={i}
-              sm={2}
-              md={2}
+              xs={12}
+              sm={4}
+              md={3}
+              lg={3}
             >
               <div
                 className='thumb'
