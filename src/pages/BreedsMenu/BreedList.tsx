5ac41e56b41f1bfa696d0ea4dc94bfc3d855b919
Thiago Cabral Carboneri <thiago.carboneri@ringa.com.br>
2022-08-06 16:01:16 -0300

    Improves HTML semantic in BreedList.tsx

--- src/pages/BreedsMenu/BreedList.tsx
+++ src/pages/BreedsMenu/BreedList.tsx
@@ -26,37 +26,40 @@ export default function BreedList() {
       <Grid
         container
       >
+        <h1
+          className='title'
+        >{breed}</h1>
         <Grid
           container
+          component='ul'
           id='cards-container'
           columnGap={0}
           rowGap={2}
         >
-          <h1
-            className='title'
-          >{breed}</h1>
           {
             picturesArr.map((picture, i) => (
               <Grid
                 item
+                component='li'
                 key={i}
                 xs={12}
                 sm={4}
                 md={3}
                 lg={3}
+                id='thumb'
+                sx={{
+                  backgroundImage: 'url(' + picture + ')',
+                }}
               >
-                <div
-                  className='thumb'
-                  style={{
-                    backgroundImage: 'url(' + picture + ')',
-                  }}
-                >
-                </div>
               </Grid>
             ))
           }
         </Grid>
       </Grid>
+      <section
+        className='overlay'
+      >
+      </section>
     </>
   );
 }
