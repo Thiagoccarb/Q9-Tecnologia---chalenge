0b237e20c6c75fd6d22ff2eb40446a98d78a2ee3
Thiago Cabral Carboneri <thiago.carboneri@ringa.com.br>
2022-08-06 10:43:21 -0300

    Implements fetchBreedList function logic when component is mounting

--- src/pages/BreedsMenu/Menu.tsx
+++ src/pages/BreedsMenu/Menu.tsx
@@ -1,9 +1,22 @@
 import * as React from 'react';
 
+import { fetchBreedList } from '../../API/list';
+
 export default function BreedsMenu() {
   const [showBreedsMenu, setShowBreedsMenu] = React.useState<boolean>(false);
+  const [breedPictures, setPictures] = React.useState<string[]>([]);
 
   const handleShowBreedsMenu = () => setShowBreedsMenu(!showBreedsMenu);
+
+  const getBreedsList = async () => {
+    const data = await fetchBreedList();
+    setPictures(data);
+  }
+
+  React.useEffect(() => {
+    getBreedsList();
+  }, []);
+
   return (
     <section
       className='menu'
