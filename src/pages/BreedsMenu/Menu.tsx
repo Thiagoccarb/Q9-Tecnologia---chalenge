022cedd62ffe3295d63950785d01c017de1c2abb
Thiago Cabral Carboneri <thiago.carboneri@ringa.com.br>
2022-08-06 01:13:50 -0300

    Implements ul display logic in Menu.tsx

--- src/pages/BreedsMenu/Menu.tsx
+++ src/pages/BreedsMenu/Menu.tsx
@@ -1,12 +1,24 @@
 import * as React from 'react';
 
 export default function BreedsMenu() {
+  const [showBreedsMenu, setShowBreedsMenu] = React.useState<boolean>(false);
+
+  const handleShowBreedsMenu = () => setShowBreedsMenu(!showBreedsMenu);
   return (
     <section
       className='menu'
     >
+      <ul
+        className={showBreedsMenu ? 'on' : ''}
+      >
+        <li>chihuahua</li>
+        <li>husky</li>
+        <li>labrador</li>
+        <li>pug</li>
+      </ul>
       <button
         className='hamburger-menu'
+        onClick={handleShowBreedsMenu}
       >
         <span></span>
         <span></span>
