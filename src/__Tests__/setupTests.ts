f31de6ece045950ca41c37de1b2450be185ecb24
Thiago Cabral Carboneri <thiago.carboneri@ringa.com.br>
2022-08-07 18:22:05 -0300

    Implements history props in renderWithRouter function in setupTest.ts

--- src/__Tests__/setupTests.ts
+++ src/__Tests__/setupTests.ts
@@ -2,14 +2,19 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { BrowserRouter } from "react-router-dom";
 
+import { createMemoryHistory } from 'history';
+
 interface RenderWithRouterProps {
   route?: string;
+  history?: any;
 }
 
-export const renderWithRouter = (ui: React.ReactElement, { route = '/' }: RenderWithRouterProps = {}) => {
+export const renderWithRouter = (ui: React.ReactElement, { route = '/', history = createMemoryHistory({ initialEntries: [route] }),
+}: RenderWithRouterProps = {}) => {
   window.history.pushState({}, 'Test page', route)
 
   return {
     ...render(ui, { wrapper: BrowserRouter }),
+    history,
   }
 }
